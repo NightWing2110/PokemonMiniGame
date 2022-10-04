b17abeeea541179b2f736dc015ce519dfa1a1e56
Duy <duylqh@adgad-net.vn>
2022-10-04 09:01:12 +0700

    PokemonMiniGame

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <manin-screen
+    v-if="statusMatch === 'default'"
+    @onStart="onHandleBeforeStart($event)"
+  />
+  <interact-screen
+    v-if="statusMatch === 'match'"
+    :cardsContext="settings.cardsContext"
+    @onFinish="onGetResult"
+  />
+  <result-screen
+    v-if="statusMatch === 'result'"
+    :timer="timer"
+    @onStartAgain="statusMatch = 'default'"
+  />
+  <copy-right />
+</template>
+
+<script>
+import ManinScreen from "./components/ManinScreen.vue";
+import InteractScreen from "./components/InteractScreen.vue";
+import ResultScreen from "./components/ResultScreen.vue";
+import CopyRight from "./components/CopyRight.vue";
+
+import { shuffled } from "./utils/array";
+export default {
+  name: "App",
+  components: {
+    ManinScreen,
+    InteractScreen,
+    ResultScreen,
+    CopyRight,
+  },
+  data() {
+    return {
+      settings: {
+        totalOfBlocks: 0,
+        cardsContext: [],
+        startedAt: null,
+      },
+      statusMatch: "default",
+      timer: 0,
+    };
+  },
+
+  methods: {
+    onHandleBeforeStart(config) {
+      console.log("Running handle before start, ", config);
+      this.settings.totalOfBlocks = config.totalOfBlocks;
+
+      const firstCards = Array.from(
+        { length: this.settings.totalOfBlocks / 2 },
+        (_, i) => i + 1
+      );
+      const secondCards = [...firstCards];
+      const cards = [...firstCards, ...secondCards];
+      this.settings.cardsContext = shuffled(
+        shuffled(shuffled(shuffled(cards)))
+      );
+      this.settings.startedAt = new Date().getTime();
+
+      //data ready
+      this.statusMatch = "match";
+    },
+    onGetResult() {
+      //get timer
+      this.timer = new Date().getTime() - this.settings.startedAt;
+      // switch to result component
+      this.statusMatch = "result";
+    },
+  },
+};
+</script>
